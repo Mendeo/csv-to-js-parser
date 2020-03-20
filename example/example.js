7927a183af9c6a6bac7fd78490b22e54b0f36cc5
Themen <thesolve@mail.ru>
2020-03-20 12:15:39 +0300

    Adding example of using.

---
+++ example/example.js
@@ -0,0 +1,27 @@
+'use strict';
+const fs = require('fs');
+const csvToObj = require('../app').csvToObj;
+const objToCsv = require('../app').objToCsv;
+
+const data = fs.readFileSync('data.csv').toString();
+
+const description =
+	{
+		customer_id: {constant: true, type: 'number', notNull: true},
+		product: {constant: false, type: 'string'},
+		name: {constant: true, type: 'string'},
+		price: {constant: false, type: 'number'},
+		closed: {constant: false, type: 'boolean'}
+	};
+
+//Csv to JavaScript object
+const obj = csvToObj(data, ';', description);
+console.log(obj);
+
+//Saving to JSON:
+const json = JSON.stringify(obj, null, ' ');
+fs.writeFileSync('data.json', json);
+
+//JavaScript object to csv
+const csv = objToCsv(obj, ';');
+fs.writeFileSync('newData.csv', csv);
