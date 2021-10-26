b36ac32c31c334257206fe07ff78790ac23e1d18
Themen <thesolve@mail.ru>
2021-10-26 10:42:49 +0300

    Add splitTiaQuotes to main app

--- app.js
+++ app.js
@@ -63,51 +63,20 @@ module.exports.csvToObj = function(data, param1, param2)
 
 	if (!delimeter) delimeter = ',';
 
-	//Spliting data by rows
-	{
-		//Checking row delimeter
-		let newRow;
-		let i = data.indexOf('\n', 0);
-		if (i === -1) throw new Error('No row delimeter found');
-		newRow = (data[i - 1] === '\r') ? '\r\n' : '\n';
-		data = data.split(newRow);
-		//Deleting last empty row
-		i = data.length - 1;
-		while (data[i] === '') 
-		{
-			data.pop();
-			i--;
-		}
-	}
-	let header = data[0].split(delimeter);
+	const dataSplited = splitTiaQuotes(data, delimeter);
+	let header = dataSplited[0];
+	data = dataSplited[1];
+
 	//Making default description
 	if (!description)
 	{
 		description = {};
 		for (let key of header)
 		{
-			description[key] = {type: 'string', group: 1};
+			description[key] = { type: 'string', group: 1 };
 		}
 	}
 
-	/*
-	console.log(delimeter);
-	console.log(description);
-	*/
-
-	//Deleting header from data
-	data.shift();
-	//Spliting data by delimeter
-	{
-		let newData = [];
-		for (let row of data)
-		{
-			let arr = row.split(delimeter);
-			newData.push(arr);
-		}
-		data = newData;
-	}
-	
 	let constantsIndexes = {};
 	let constantOrder = [];
 	let arraysIndexes = {};
@@ -288,6 +257,170 @@ module.exports.csvToObj = function(data, param1, param2)
 				};
 			default:
 				throw new Error('Type is incorrect');
+		}
+	}
+	//Split by delimeter, taking into account double quotes according to rfc4180
+	function splitTiaQuotes(data, delimeter)
+	{
+		const dataArray = [];
+		let header = null;
+		let rowArray = [];
+		let dataIndex = 0;
+		let qPlaceOpen = 0;
+		let dOrnPlace = 0;
+		let dPlace = 0;
+		let rnPlace = 0;
+		let qPlaceClosed = -1;
+		for (;;)
+		{
+			if (dPlace >= 0 && dPlace <= dOrnPlace) dPlace = data.indexOf(delimeter, dataIndex);
+			if (rnPlace >= 0 && rnPlace <= dOrnPlace) rnPlace = data.indexOf('\n', dataIndex);
+			set_dOrnPlace();
+			if (qPlaceOpen <= dOrnPlace)
+			{
+				qPlaceOpen = data.indexOf('"', dataIndex);
+				qPlaceClosed = qPlaceOpen === -1 ? -1 : data.indexOf('"', qPlaceOpen + 1);
+			}
+			if (dOrnPlace === -1) //last field
+			{
+				if (qPlaceOpen === -1) //last field without quotes
+				{
+					rowArray.push(data.slice(dataIndex, data.length));
+					addRowToArray();
+					break;
+				}
+				else if(qPlaceClosed > qPlaceOpen) //last field has more then one quotes
+				{
+					while (data.slice(qPlaceClosed + 1, qPlaceClosed + 2) === '"') //qPlaceClosed refer to escape simbol of "
+					{
+						qPlaceClosed = data.indexOf('"', qPlaceClosed + 2);
+					}
+					for (let i = qPlaceClosed + 1; i < data.length; i++) //After closing quotes and before delimeter we have not space simbols
+					{
+						if (data.slice(i, i + 1) !== ' ') throw new Error('Incorrect using of quotes (1): ' + data.slice(qPlaceOpen, data.length));
+					}
+					rowArray.push(data.slice(qPlaceOpen + 1, qPlaceClosed).replace(/""/g, '"'));
+					addRowToArray();
+					break;
+				}
+				else //last field has only one qoute
+				{
+					rowArray.push(data.slice(dataIndex, data.length));
+					addRowToArray();
+					break;
+				}
+			}
+			else if (qPlaceOpen === -1) //no quotes in field;
+			{
+				rowArray.push(data.slice(dataIndex, dOrnPlace));
+				dataIndex = dOrnPlace + 1;
+				addRowToArray();
+			}
+			else if (qPlaceOpen < dOrnPlace) //field has quotes
+			{
+				let fieldStartsFromQuote;
+				if (qPlaceOpen === dataIndex)
+				{
+					fieldStartsFromQuote = true;
+				}
+				else
+				{
+					fieldStartsFromQuote = true;
+					for (let i = dataIndex; i < qPlaceOpen; i++)
+					{
+						if (data.slice(i, i + 1) !== ' ')
+						{
+							fieldStartsFromQuote = false;
+							break;
+						}
+					}
+				}
+				if (fieldStartsFromQuote) //filed start from quote
+				{
+					for (;;)
+					{
+						if (data.slice(qPlaceClosed + 1, qPlaceClosed + 2) === '"') //qPlaceClosed refer to escape simbol of "
+						{
+							qPlaceClosed = data.indexOf('"', qPlaceClosed + 2);
+						}
+						else
+						{
+							if (dOrnPlace < qPlaceClosed) //dOrnPlace refer to delimeter into escaped filed, for example ""aa, aa
+							{
+								if (dPlace < qPlaceClosed) dPlace = data.indexOf(delimeter, qPlaceClosed + 1);
+								if (rnPlace < qPlaceClosed) rnPlace = data.indexOf('\n', qPlaceClosed + 1);
+								set_dOrnPlace();
+							}
+							break;
+						}
+					}
+					for (let i = qPlaceClosed + 1; i < dOrnPlace; i++) //After closing quotes and before delimeter we have not space simbols
+					{
+						if (data.slice(i, i + 1) !== ' ') throw new Error('Incorrect using of quotes (2): ' + data.slice(qPlaceOpen, qPlaceClosed + 1));
+					}
+					rowArray.push(data.slice(qPlaceOpen + 1, qPlaceClosed).replace(/""/g, '"'));
+					dataIndex = dOrnPlace + 1;
+					addRowToArray();
+				}
+				else //filed has quote, but not start from quote
+				{
+					rowArray.push(data.slice(dataIndex, dOrnPlace));
+					dataIndex = dOrnPlace + 1;
+					addRowToArray();
+				}
+			}
+			else //filed has not quotes
+			{
+				rowArray.push(data.slice(dataIndex, dOrnPlace));
+				dataIndex = dOrnPlace + 1;
+				addRowToArray();
+			}
+		}
+		//Deleting last empty rows
+		{
+			let i = dataArray.length - 1;
+			while (dataArray[i].length === 1 && dataArray[i][0] === '')
+			{
+				dataArray.pop();
+				i--;
+			}
+		}
+		return [header, dataArray];
+
+		function addRowToArray()
+		{
+			if (dOrnPlace === rnPlace)
+			{
+				if (header === null)
+				{
+					header = rowArray;
+				}
+				else
+				{
+					dataArray.push(rowArray);
+				}
+				rowArray = [];
+			}
+		}
+
+		function set_dOrnPlace()
+		{
+			if (dPlace === -1)
+			{
+				dOrnPlace = rnPlace;
+			}
+			else if (rnPlace === -1)
+			{
+				dOrnPlace = dPlace;
+			}
+			else if (rnPlace < dPlace)
+			{
+				dOrnPlace = rnPlace;
+			}
+			else
+			{
+				dOrnPlace = dPlace;
+			}
 		}
 	}
 };
